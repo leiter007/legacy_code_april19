54194ce5c70a0399943f2e9243afac1433494b38
Carrosen <carla1.rosen@gmail.com>
2019-04-24 21:40:02 +0200

    adds step definitions from feature test snippets writes the first step definition for background first test green

---
+++ features/step_definitions/user_can_create_account_steps.rb
@@ -0,0 +1,24 @@
+Given("I visit the {string} page") do |string|
+    visit root_path
+end
+  
+  When("I click {string} link") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I fill in {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should be on {string} page") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should see {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
