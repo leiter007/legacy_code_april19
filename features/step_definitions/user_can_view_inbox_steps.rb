20466db0b6c790a6fcd86e1d2d9f8744028945f2
raptorf1 <raptor_f1@hotmail.com>
2019-04-25 15:23:49 +0200

    Redifines the method syntax in user_can_view_steps.rb. 10/10 tests of scenario green.

--- features/step_definitions/user_can_view_inbox_steps.rb
+++ features/step_definitions/user_can_view_inbox_steps.rb
@@ -1,6 +1,6 @@
-Given("the following user exists") do |table|
+  Given("the following user exists") do |table|
     table.hashes.each do |user|
-        User.create!(user)
+      User.create!(user)
     end    
   end
   
@@ -9,15 +9,15 @@ Given("the following user exists") do |table|
   end
   
   Then("I click on the {string} link") do |login_link|
-    click_link(login_link)
+    click_link login_link
   end
   
-  Then("I fill in the {string} field with {string}") do |user_email, email|
-    page.fill_in "user_email", with: "email"
+  Then("I fill in the {string} field with {string}") do |field, user_data|
+    fill_in field, with: user_data
   end
-  
+
   Then("I click {string} button") do |login|
-    click_button(login)
+    click_button login
   end
  
   Then("I should see {string}") do |content|
