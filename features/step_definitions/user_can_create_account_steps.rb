136cc4d0376b9ae223bd506cb5d7fdac87f8ac83
felix_bonnier <felix_bonnier@yahoo.com>
2019-04-25 10:59:38 +0200

    adds test for checking hello message using page.has_content method. Test is green

--- features/step_definitions/user_can_create_account_steps.rb
+++ features/step_definitions/user_can_create_account_steps.rb
@@ -19,6 +19,5 @@ end
   end
   
   Then("I should see {string}") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+    page.has_content? string
   end
-  
